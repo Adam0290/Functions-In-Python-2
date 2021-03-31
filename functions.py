370b06987cd19087bfe94dee2dcaca8a55340684
Adam_Codes <76964302+Adam0290@users.noreply.github.com>
2021-03-31 18:34:28 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,22 +1,4 @@
-coffee_is_grinding = True
-
-# def press_grind_beans():
-#     global coffee_is_grinding
-#     if coffee_is_grinding == True:
-#         print("Stopping the grind")
-#         coffee_is_grinding = False
-#     elif coffee_is_grinding == False:
-#         print("Grinding is about to begin")    
-#         coffee_is_grinding = True
-
-# coffee_is_grinding = False
-# press_grind_beans()
-# print(coffee_is_grinding)    
-
-
-
-# Functions 
-balance = 1000
+ balance = 1000
 
 def withdaw_funds(amount, account_number):
     global balance
